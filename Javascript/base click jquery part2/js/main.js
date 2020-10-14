3e2b9b0dff5beb150668206a4dbd5f9fd5a29b60
Joel GODENIR <joel.devweb@gmail.com>
2020-10-14 09:18:54 +0200

    jquery

---
+++ Javascript/base click jquery part2/js/main.js
@@ -0,0 +1,11 @@
+$('.cercle').click (function(){
+
+    $(this).animate({height:"400px", width:"400px"},2000);
+    
+});
+
+$(".cercle2").click (function(){
+
+    $(this).addClass("modif");
+});
+
